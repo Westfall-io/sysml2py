24b87baef60298055dfc0d57f20abaa8a9a7a948
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2023-05-31 22:44:43 -0400

    🤡 Fix to path?

--- src/sysml2py/__init__.py
+++ src/sysml2py/__init__.py
@@ -75,10 +75,10 @@ def loads(s):
     if not isinstance(s, str):
         raise TypeError(f"the SysML object must be str, " f"not {s.__class__.__name__}")
 
-    #try:
-    stream = pkg_resources.resource_stream(__name__, 'grammar/KerMLExpressions.tx')
-    #except:
-    #    stream = pkg_resources.resource_stream(__name__, 'src/sysml2py/grammar/KerMLExpressions.tx')
+    try:
+        stream = pkg_resources.resource_stream(__name__, 'grammar/KerMLExpressions.tx')
+    except:
+        stream = pkg_resources.resource_stream(__name__, 'src/sysml2py/grammar/KerMLExpressions.tx')
     grammar = stream.read().decode('utf-8')
     meta = metamodel_from_str(grammar)
 
