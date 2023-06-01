208cdd62db514b466b0b64aa70134e54cea46ce0
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2023-06-01 08:13:56 -0400

    :memo: Updates to version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Christopher Cox"
 author = "Christopher Cox"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
