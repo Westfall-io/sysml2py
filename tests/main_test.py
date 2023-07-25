400078b5398462758acc353c6a3ba08ad8920f93
chriscox-westfall <chriscox-westfall@users.noreply.github.com>
2023-07-25 03:40:22 +0000

    :robot: Format code with black

--- tests/main_test.py
+++ tests/main_test.py
@@ -22,9 +22,11 @@ def test_grammar_load_fromfile(single_package):
     grammar = load_grammar(f)
     assert strip_ws(classtree(grammar).dump()) == strip_ws(single_package)
 
+
 def test_grammar_invalid_input():
     with pytest.raises(TypeError):
         load_grammar({})
+
 
 def test_load_fromfile(single_package):
     with open("temp.txt", "w") as f:
@@ -35,13 +37,16 @@ def test_load_fromfile(single_package):
     model = load(f)
     assert strip_ws(model.dump()) == strip_ws(single_package)
 
+
 def test_load_fromfile_error(single_package):
     with pytest.raises(TypeError):
-        load('string')
+        load("string")
+
 
 def test_load_fromstr(single_package):
     model = loads(single_package)
     assert strip_ws(model.dump()) == strip_ws(single_package)
+
 
 def test_load_fromstr_error(single_package):
     with pytest.raises(TypeError):
